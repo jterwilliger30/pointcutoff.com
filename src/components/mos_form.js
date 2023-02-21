6f2c63797d5b790a9682d0751010667ce810347d
EC2 Default User <ec2-user@ip-172-31-11-101.us-west-1.compute.internal>
2023-02-21 19:34:11 +0000

    Small changes from prod server

--- src/components/mos_form.js
+++ src/components/mos_form.js
@@ -37,8 +37,12 @@ function Create() {
 
         </input>
         <button type="submit">Submit</button>
-        <h1>Current results are for the month of <span class="month">March, 2023</span>.</h1>
+        <h1>Current results are for <span class="month">March, 2023</span>.</h1>
         <p id="demo"></p>
+	<p id="footer">
+	<a href="https://github.com/jterwilliger30/pointcutoff.com">GitHub Repo</a>  <br />
+	<a href="mailto:jterwilliger30@gmail.com">Email for questions & feature requests</a>
+	</p>
         </form>
 
         
