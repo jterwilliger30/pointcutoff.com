f774b9d2911371dfed95fdae289ae7f04d9df735
Jack Terwilliger <jterwilliger30@gmail.com>
2023-04-18 09:43:49 -0700

    Remove dead code from old parser

--- api/app.py
+++ api/app.py
@@ -6,29 +6,6 @@ a = Parse("./pdfs/Apr23.pdf")
 a.read_pdf_pages([4,5,6], [7,8,9])
 
 dict = a.create_mos_dict()
-"""
-
-
-
-text = extract_text("./Apr23.pdf")
-matches = []
-
-
-text = text.split("\n")
-
-for i in text:
-    print(i)
-
-dict = {}
-
-for i in range(len(matches)):
-    if len(matches[i]) == 13:
-        dict[matches[i][0]] = {"SGT PZ: ":  matches[i][1] , "SGT SZ: ": matches[i][2], "SSG PZ: ": matches[i][3], "SSG SZ: ": matches[i][4]}
-    elif len(matches[i]) == 7:
-        dict[matches[i][0]] = {"SGT PZ: ":  matches[i][1] , "SGT SZ: ": matches[i][1], "SSG PZ: ": matches[i][2], "SSG SZ: ": matches[i][2]}
-"""
-        
-
 
 
 app = Flask(__name__, template_folder='./')
