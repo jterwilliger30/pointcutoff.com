ccef9b8b9f8a937d591f983f8e3e7b588a84f56b
Jack Terwilliger <jterwilliger30@gmail.com>
2023-04-20 13:46:37 -0700

    Incorporated AC/AGR selector logic

--- api/app.py
+++ api/app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
 
-from classes.parse import Parse
+from classes.parse import AC_Parse, AGR_Parse
 
-a = Parse("./pdfs/Apr23.pdf")
+pdf_name = "Apr23.pdf"
+
+a = AC_Parse("./pdfs/ac/" + str(pdf_name))
 a.read_extract()
+ac_dict = a.create_mos_dict()
 
-
-dict = a.create_mos_dict()
-
+b = AGR_Parse("./pdfs/agr/" + str(pdf_name))
+b.read_extract()
+agr_dict = b.create_mos_dict()
 
 app = Flask(__name__, template_folder='./')
 
 @app.route('/handle_data', methods=['GET', 'POST'])
 def handle_data():
     json = request.get_json()
+
     key = json['data']
-    try:
-        return dict[key.upper()]
-    except:
+    ac_agr = json['selector']
+
+    if ac_agr == "ac":
+        try:
+            return ac_dict[key.upper()]
+        except:
+            return
+    elif ac_agr == "agr":
+        try:
+            return agr_dict[key.upper()]
+        except:
+            return
+    else:
         return
